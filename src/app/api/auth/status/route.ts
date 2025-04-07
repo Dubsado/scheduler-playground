7502c15a69c52614fe7f7f1f42b97aeff8bd8c72
Daniel Di Venere <danieldivenere@gmail.com>
2025-04-07 16:55:08 -0700

    always logged in

--- src/app/api/auth/status/route.ts
+++ src/app/api/auth/status/route.ts
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   // For now, returning a hardcoded value
-  return NextResponse.json({ isLoggedIn: false });
+  return NextResponse.json({ isLoggedIn: true });
 }
